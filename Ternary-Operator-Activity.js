0208a7aab8555c47596bf36de858fd22d204be58
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2023-01-23 04:47:10 -0500

    Create Ternary-Operator-Activity.js

---
+++ Ternary-Operator-Activity.js
@@ -0,0 +1,21 @@
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log('I don\'t love that!');
+
+// In this activity I learned that a ternary operator can be used to simplify an if...else statement by basically shortening it
+// with better syntax. Then in the actvity itself i was tasked with taking elongated if...else statements and using ternary operators for
+// better syntax
+
+// Actvity Instructions:
+
+// 1.) Refactor, or edit, the first if...else block to use a ternary operator.
+// 2.) Refactor the second if...else block to use a ternary operator.
+// 3.) Refactor the third if...else block to use a ternary operator. 
