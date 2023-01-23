719b28233ddf3dbbdc761a5a8a1561393e2a7c8b
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2023-01-23 02:38:27 -0500

    Create Truthy-And-Falsy-Activity.js

---
+++ Truthy-And-Falsy-Activity.js
@@ -0,0 +1,30 @@
+let wordCount = 10;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+// In this activity I worked with the concept of Truthy and Falsy and how they relate to conditional statements. I learned 
+// how these concepts are used in if...else statements depending on the truthy or falsy value and how they trigger the if statements or the else statements.
+// Also learned defintions of turthy and falsy values. 
+// Lastly I did an actvity that involved changing the value of a variable so it is "truthy" and triggers the "if" statement. Then I changed the value of
+// another variable so that it was still a string but had a "falsy" value so the else statement was triggered
+
+// Instructions for Actvity as follows:
+
+// 1.) Change the value of wordCount so that it is truthy. This value should still be a number.
+// After you make this change and run your code, 'Great! You've started your work!' should log to the console.
+
+// 2.) Change the value of favoritePhrase so that it is still a string but falsy.
+// After you make this change and run your code, 'This string is definitely empty.' should log to the console.
