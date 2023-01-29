3277a84318d27af297cfabae2ee34630d94dbb98
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2023-01-29 09:30:17 -0500

    Create Magic-8-Ball.js

---
+++ Magic-8-Ball.js
@@ -0,0 +1,47 @@
+let userName = "Matthew";
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello")
+
+const userQuestion = "Will the 49ers win the Super Bowl?"
+console.log(userQuestion)
+
+randomNumber = Math.floor(Math.random() * 8);
+
+eightBall = "";
+switch (randomNumber) {
+  case "0":
+    console.log('It is certain');
+    break;
+
+  case 1:
+  console.log('It is decidedly so');
+  break;
+
+  case 2:
+  console.log('Reply hazy try again');
+  break;
+
+  case 3:
+  console.log('Cannot predict now');
+  break;
+
+  case 4:
+  console.log('Do not count on it');
+  break;
+
+  case 5:
+  console.log('My sources say no');
+  break;
+
+   case 6:
+  console.log('Outlook not so good');
+  break;
+
+ case 7:
+  console.log('Signs point to yes');
+  break;
+
+}
+
+  console.log(eightBall)
+
+// This project is the creation of a "Magic 8-Ball Project" it uses the control flow theory(order and process of how code is executed)
