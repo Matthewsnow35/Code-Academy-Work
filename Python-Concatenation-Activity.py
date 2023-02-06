d922e01e27e97bfe5281a1026ed95fbd8c9a9498
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2023-02-06 04:37:50 -0500

    Create Python-Concatenation-Activity.py

---
+++ Python-Concatenation-Activity.py
@@ -0,0 +1,23 @@
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6
+print(message)
+#print(message)
+
+
+# In this activity I learned about String Concatenation in Python. I learned that string concatenation is when two or more strings are combined. 
+# Performing string concatenation creates a brand new string comprised of the first string’s contents followed by the second string’s contents.
+# I also learned how to concatenate a string with a number, thats done by making number a string first, then using the str() function.
+
+# In the activity I was tasked with taking the strings provided, concatenate them into a variable and printing them to the console
+
+# ACTIVITY INSTRUCTIONS: 
+# 1.) Concatenate the strings and save the message they form in the variable message.
+
+# 2.) Now uncomment the print statement and run your code to see the result in the terminal!
