e5a4c86179b2420cd9d2617f5b357845e1071a11
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2023-02-19 02:54:58 -0500

    Create Tree-Java-Activity.java

---
+++ Tree-Java-Activity.java
@@ -0,0 +1,49 @@
+public class Tree {
+  public static void main(String[] args) {
+    // Hi, I am Matthew and I will print a tree to the screen
+    System.out.println("Hey there, I'm Matthew and I'm learning to code in Java!");
+    System.out.println("I am planting a tree today!");
+    System.out.println("******");
+    System.out.println(" ****");
+    System.out.println("  **");
+ 
+  }
+  // Define your tree class in this file!
+}
+
+
+// *****Activity Directions*****
+
+// 1.) We’re starting with a blank Java file named Tree.java.
+//    -Define a class that follows the Java naming conventions.
+
+// 2.) This code produces an error because our program needs a main() method.
+//    -Define this method inside the curly braces of the Tree class.
+
+// 3.) Write a comment in main() that describes the task it will perform.
+//    -We’re going to introduce ourselves and print a tree to the screen.
+//    -You can use the single-line or multi-line syntax for your comment.
+
+// 4.) Below your comment, print a message introducing yourself to the user.
+//    -Try something like “Hey there, I’m Ariel and I’m learning to code in Java!”
+//    -When you run the code, you should see this message printed to the screen.
+
+// 5.)After introducing yourself, use another print statement to output the project goal!
+//    -Something like “I’m going to plant a tree today!” or “Ready to get my hands dirty!”
+
+// 6.) See how the second statement begins on a new line? We’ll use multiple print statements to plant our tree.
+// For example:
+
+//    System.out.println("  *  ");
+//    System.out.println(" *** ");
+//    System.out.println(" *** ");
+//    System.out.println("  *  ");
+//    System.out.println("  *  ");   
+//  will print a tree like this:
+
+  *  
+ *** 
+ *** 
+  *  
+  *  
+// Try it out!
